a7bcdf1368a7b22a3c2230f27ddfbab7b40a4505
Madanraj1 <madanraj251999@gmail.com>
2022-07-23 19:51:06 +0530

    login screen otp ui done

--- lib/UI/Auth/loginScreen.dart
+++ lib/UI/Auth/loginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_field.dart';
+import 'package:otp_text_field/style.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -9,7 +11,10 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   TextEditingController? _phoneNumberFieldController;
+  OtpFieldController otpController = OtpFieldController();
+
   FocusNode? _phoneFocusNode;
+  bool otpSent = false;
 
   @override
   void initState() {
@@ -17,14 +22,6 @@ class _LoginScreenState extends State<LoginScreen> {
     _phoneFocusNode = FocusNode();
 
     super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    // FocusScope.of(context).requestFocus(_phoneFocusNode);
-    // _phoneFocusNode!.requestFocus();
-
-    super.didChangeDependencies();
   }
 
   @override
@@ -53,99 +50,181 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             SizedBox(height: _height * 0.05),
-            Container(
-              height: _height * 0.4,
-              width: _width * 0.8,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(2, 2),
-                    blurRadius: 12,
-                    color: Color.fromRGBO(0, 0, 0, 0.16),
-                  )
-                ],
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    "Enter Mobile Number to Continue",
-                    style: TextStyle(
-                      fontSize: _height * 0.02,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Container(
-                    width: _width * 0.7,
-                    height: _height * 0.06,
+            // phone number container
+            otpSent == false
+                ? Container(
+                    height: _height * 0.4,
+                    width: _width * 0.8,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey[200],
-                    ),
-                    alignment: Alignment.center,
-                    child: TextField(
-                      autofocus: true,
-                      // maxLength: 10,
-                      focusNode: _phoneFocusNode,
-                      controller: _phoneNumberFieldController,
-                      keyboardType: TextInputType.phone,
-                      onChanged: (value) {},
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
                       ),
-                      cursorColor: Colors.white,
-                      textAlign: TextAlign.center,
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-                        prefixText: '+91',
-                        prefixStyle: TextStyle(
-                          color: Colors.grey[700],
-                          fontSize: 15,
-                        ),
-                        hintText: "Enter Mobile Number",
-                        hintStyle: TextStyle(
-                          color: Colors.grey[700],
-                          fontSize: 15,
-                        ),
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: _width * 0.35,
-                    height: _height * 0.05,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Color(0xff3E66FB),
-                    ),
-                    alignment: Alignment.center,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Continue",
-                          style: TextStyle(
-                            fontSize: _height * 0.02,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Icon(
-                          Icons.navigate_next_outlined,
-                          color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 12,
+                          color: Color.fromRGBO(0, 0, 0, 0.16),
                         )
                       ],
                     ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Enter Mobile Number to Continue",
+                          style: TextStyle(
+                            fontSize: _height * 0.02,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Container(
+                          width: _width * 0.7,
+                          height: _height * 0.06,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey[200],
+                          ),
+                          alignment: Alignment.center,
+                          child: TextField(
+                            autofocus: true,
+                            // maxLength: 10,
+                            focusNode: _phoneFocusNode,
+                            controller: _phoneNumberFieldController,
+                            keyboardType: TextInputType.phone,
+                            onChanged: (value) {},
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.black,
+                            ),
+                            cursorColor: Colors.white,
+                            textAlign: TextAlign.center,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                              prefixText: '+91',
+                              prefixStyle: TextStyle(
+                                color: Colors.grey[700],
+                                fontSize: 15,
+                              ),
+                              hintText: "Enter Mobile Number",
+                              hintStyle: TextStyle(
+                                color: Colors.grey[700],
+                                fontSize: 15,
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              otpSent = true;
+                            });
+                          },
+                          child: Container(
+                            width: _width * 0.35,
+                            height: _height * 0.05,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0xff3E66FB),
+                            ),
+                            alignment: Alignment.center,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Continue",
+                                  style: TextStyle(
+                                    fontSize: _height * 0.02,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.navigate_next_outlined,
+                                  color: Colors.white,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                // otp container
+                : Container(
+                    height: _height * 0.4,
+                    width: _width * 0.8,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(2, 2),
+                          blurRadius: 12,
+                          color: Color.fromRGBO(0, 0, 0, 0.16),
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Enter the OTP sent to this number +911122334455",
+                          style: TextStyle(
+                            fontSize: _height * 0.017,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        OTPTextField(
+                            controller: otpController,
+                            length: 6,
+                            width: MediaQuery.of(context).size.width * 0.73,
+                            textFieldAlignment: MainAxisAlignment.spaceAround,
+                            fieldWidth: 45,
+                            fieldStyle: FieldStyle.box,
+                            outlineBorderRadius: 15,
+                            style: TextStyle(fontSize: 17),
+                            onChanged: (pin) {
+                              print("Changed: " + pin);
+                            },
+                            onCompleted: (pin) {
+                              print("Completed: " + pin);
+                            }),
+                        Container(
+                          width: _width * 0.35,
+                          height: _height * 0.05,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Color(0xff3E66FB),
+                          ),
+                          alignment: Alignment.center,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Resend Otp",
+                                style: TextStyle(
+                                  fontSize: _height * 0.02,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Icon(
+                                Icons.sms,
+                                color: Colors.white,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
             SizedBox(height: _height * 0.1),
           ],
         ),
